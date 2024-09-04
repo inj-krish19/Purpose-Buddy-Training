a811c04df9c7283aa4881b46e2a5e0a4e8ce639a
inj-krish19 <kglivee19@gmail.com>
2024-09-04 15:51:02 +0530

    Making Demo Controller and Rendering Hello World at Index page

---
+++ Purpose-Buddy/app/Http/Controllers/DemoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    // This is a controller that is made using command
+    // php artisan make:controller DemoController
+    // here we write access to hello world
+
+    public function index(){
+        return view("hello");
+    }
+
+
+}
